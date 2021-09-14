171bf6973d10529ac5cd5c81d113f778aaa55786
Max Flow O2 <MaxflowO2@maxflowO2.com>
2021-09-14 22:02:55 +0000

    Added ERC165Storage + constructor() to set bytes4 code

--- contracts/ERC2981.sol
+++ contracts/ERC2981.sol
@@ -14,14 +14,25 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Storage.sol";
 import "./IERC2981.sol";
 
-abstract contract ERC2981 is IERC2981 {
+abstract contract ERC2981 is IERC2981, ERC165Storage {
   using SafeMath for uint256;
+
+  // Bytes4 Code for EIP-2981
+  bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
 
   // Mappings _tokenID -> values
   mapping(uint256 => address) receiver;
   mapping(uint256 => uint256) royaltyPercentage;
+
+  constructor() {
+
+    // Using ERC165Storage set EIP-2981
+    _registerInterface(_INTERFACE_ID_ERC2981);
+
+  }
 
   // Set to be internal function _setReceiver
   function _setReceiver(uint256 _tokenId, address _address) internal {
