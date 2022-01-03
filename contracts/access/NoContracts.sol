f4741d88c88caa863da946d9d2bdaf0e811513a0
Max Flow O2 <MaxflowO2@D0F2930>
2022-01-03 05:27:59 +0000

    v2.1.4 comment cleaning

--- contracts/access/NoContracts.sol
+++ contracts/access/NoContracts.sol
@@ -28,8 +28,7 @@ import "../utils/ContextV2.sol";
 
 /**
  * @dev Contract module which provides a basic access control mechanism, where
- * there is an exclusion (any contracts) that can be granted exclusion to
- * specific functions.
+ * there is an exclusion (any contracts) that can be on specific functions.
  *
  * This module is used through inheritance. It will make available the modifier
  * `noContracts()`, which can be applied to your functions to restrict their use.
@@ -40,13 +39,57 @@ abstract contract NoContracts is ContextV2 {
 
   modifier noContracts() {
     require(!_msgSender().isContract(), "Caller is a contract");
+
     /**
-     * @dev this is check one, see Address.sol about warning
+     * @dev Returns true if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * It is unsafe to assume that an address for which this function returns
+     * false is an externally-owned account (EOA) and not a contract.
+     *
+     * Among others, `isContract` will return false for the following
+     * types of addresses:
+     *
+     *  - an externally-owned account
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     *
+     * [IMPORTANT]
+     * ====
+     * You shouldn't rely on `isContract` to protect against flash loan attacks!
+     *
+     * Preventing calls from contracts is highly discouraged. It breaks composability, breaks support for smart wallets
+     * like Gnosis Safe, and does not provide security since it can be circumvented by calling from a contract
+     * constructor.
+     * ====
      */
+
     require(_msgSender() == _txOrigin(), "Caller is a contract");
+
     /**
-     * @dev this is check two, killing the constructor hacks
+     * @dev Returns false if `account` is a contract.
+     *
+     * [IMPORTANT]
+     * ====
+     * By using msg.sender and tx.origin and ensuring they are equal, this
+     * as of solidity 0.8.11 will ensure the call was made by an EOA.
+     *
+     * This will prevent
+     *  - a contract in construction
+     *  - an address where a contract will be created
+     *  - an address where a contract lived, but was destroyed
+     * ====
+     *
+     * [IMPORTANT]
+     * ====
+     * Preventing calls from contracts is highly discouraged. It breaks composability, breaks support for smart wallets
+     * like Gnosis Safe.
+     * ====
      */
+
     _;
   }
 }
